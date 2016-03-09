74677714ed3c022e21716ced3d18c844b2ab1792
Valerie Wilson <valeriewilson@Valeries-MacBook-Pro.local>
2016-03-08 21:06:38 -0800

    Adding basic epic statistics + making json keys clearer

--- sandglass.py
+++ sandglass.py
@@ -6,6 +6,10 @@
 #   3. Use the information obtained through the prompt to display epic information in a GUI
 
 import json
+import numpy as np
+import datetime as dt
+from datetime import date
+import time
 
 with open("projects.txt") as project_file:
 	project_file = project_file.read()
@@ -26,16 +30,16 @@ while(True):
 		print "--New entry--"
 		print "Epic link: " + l
 		if n != "":
-			projects[l].update({"n":n})
+			projects[l].update({"epic_name":n})
 			print "Project name: " + n
 		if s != "":
-			projects[l].update({"s":s})
+			projects[l].update({"start_date":s})
 			print "Start date: " + s
 		if e != "":
-			projects[l].update({"e":e})
+			projects[l].update({"exp_end_date":e})
 			print "Planned end date: " + e
 		if a != "":
-			projects[l].update({"a":a})
+			projects[l].update({"act_end_date":a})
 			print "Actual end date: " + a
 	
 	elif response == "e":
@@ -45,22 +49,22 @@ while(True):
 		print "----------------"
 		for l in epic_list:
 			print "Epic link: " + l
-			if "n" in projects[l]:
-				print "Project name: " + projects[l]["n"]
-			if "s" in projects[l]:
-				print "Start date: " + projects[l]["s"]
-			if "e" in projects[l]:
-				print "Planned end date: " + projects[l]["e"]
-			if "a" in projects[l]:
-				print "Actual end date: " + projects[l]["a"]
+			if "epic_name" in projects[l]:
+				print "Project name: " + projects[l]["epic_name"]
+			if "start_date" in projects[l]:
+				print "Start date: " + projects[l]["start_date"]
+			if "exp_end_date" in projects[l]:
+				print "Expected end date: " + projects[l]["exp_end_date"]
+			if "act_end_date" in projects[l]:
+				print "Actual end date: " + projects[l]["act_end_date"]
 			print "----------------"
 		
 		# Prompt user for the field to edit and display current entry
 		edit_project = raw_input("Which epic would you like to edit? (type the epic link): ")
-		edit_field = raw_input('Which field would you like to add or update? (type "n" for project name, "s" for start date, "e" for planned end date, or "a" for actual end date: ')
+		edit_field = raw_input('Which field would you like to add or update - epic_name, start_date, exp_end_date, or act_end_date?: ')
 		
 		# Allow the user to input the new/updated value, save to projects variable, and output value
-		if edit_field in ("n","s","e","a"):
+		if edit_field in ("epic_name","start_date","exp_end_date","act_end_date"):
 			if edit_field in projects[edit_project].keys():
 				print "Current value: %s" % projects[edit_project][edit_field]
 				new_value = raw_input("What is the new value?: ")
@@ -75,6 +79,27 @@ while(True):
 		break
 	else:
 		print "Invalid input. Please try again."
+
+print "Epic information / stats"
+epic_list = projects.keys()
+for l in epic_list:
+	if "epic_name" in projects[l]:
+		print "Project name: " + projects[l]["epic_name"]
+	if "start_date" in projects[l]:
+		print "Start date: " + projects[l]["start_date"]
+	if "exp_end_date" in projects[l]:
+		print "Expected end date: " + projects[l]["exp_end_date"]
+	if "act_end_date" in projects[l]:
+		print "Actual end date: " + projects[l]["act_end_date"]
+	if "start_date" in projects[l] and "exp_end_date" in projects[l]:
+		start = dt.date(int(projects[l]["start_date"].split("/")[2]), int(projects[l]["start_date"].split("/")[0]), int(projects[l]["start_date"].split("/")[1]))
+		end = dt.date(int(projects[l]["exp_end_date"].split("/")[2]), int(projects[l]["exp_end_date"].split("/")[0]), int(projects[l]["exp_end_date"].split("/")[1]))
+		print "Expected business days elapsed: %i" % np.busday_count(start,end)
+	if "start_date" in projects[l] and "act_end_date" in projects[l]:
+		start = dt.date(int(projects[l]["start_date"].split("/")[2]), int(projects[l]["start_date"].split("/")[0]), int(projects[l]["start_date"].split("/")[1]))
+		end = dt.date(int(projects[l]["act_end_date"].split("/")[2]), int(projects[l]["act_end_date"].split("/")[0]), int(projects[l]["act_end_date"].split("/")[1]))
+		print "Actual business days elapsed: %i" % np.busday_count(start,end)
+	print "----------------"
 
 with open("projects.txt","w") as project_file:
 	# Update projects.txt with new content
